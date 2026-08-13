e777135ffee3b07223902995dc1251d69c55f35d
ytbro8326-sudo <ytbro8326-sudo@users.noreply.github.com>
2026-08-13 19:54:22 +0600

    Configure AnimeGG auto referer https://www.animegg.org/ and local python proxy server

--- app.py
+++ app.py
@@ -43,8 +43,8 @@ SESSION.verify = False
 
 DEFAULT_PROXY_HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
-    "Referer": "https://megaplay.buzz/",
-    "Origin": "https://megaplay.buzz",
+    "Referer": "https://www.animegg.org/",
+    "Origin": "https://www.animegg.org",
     "Accept": "*/*",
     "Accept-Language": "en-US,en;q=0.9",
     "Sec-Fetch-Site": "cross-site",
